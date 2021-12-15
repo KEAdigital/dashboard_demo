aaefd28c8480583e17667d5423aa23241734bf0c
Tue Hellstern <tueh@kea.dk>
2021-12-15 13:06:23 +0100

    Add files via upload

---
+++ app_2.py
@@ -0,0 +1,128 @@
+#
+# Imports Dash
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+
+# Imports Div.
+import pandas as pd
+import numpy as np
+import openpyxl
+import calendar
+
+# Imports Plotly
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+# ***************************************
+# Get data
+# ***************************************
+import datamodel
+order = datamodel.get_data()
+df_year = datamodel.get_year()
+df_month = datamodel.get_month()
+
+
+# ***************************************
+# Diagram - Employee Sales
+# ***************************************
+fig_employee = px.bar(order, 
+    x='emp_name', y='total', 
+    color='type', text='total', title='Sales by Employee',
+    hover_data=[],
+    labels={'total':'Total sales', 'emp_name':'Employee', 'type':'Product Type'})
+fig_employee.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+fig_employee.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
+
+
+# ***************************************
+# Activate the app
+# ***************************************
+app = dash.Dash(__name__)
+
+# ***************************************
+# Layout
+# ***************************************
+app.layout = html.Div(
+    children=[
+        html.Div(className='row',
+                children=[
+                    html.Div(className='four columns div-user-controls',
+                            children=[
+                                html.H2('Sales dashboard'),
+                                html.P('Select filters from dropdown'),
+
+                    html.Div(children="Month", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_month',
+                                options=[{'label':selectmonth, 'value':selectmonth} for selectmonth in df_month['monthnames']],
+                            ),
+                    html.Div(children="Year", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_year',
+                                options=[{'label':selectyear, 'value':selectyear} for selectyear in df_year]
+                            ),
+                            ]
+                    ),
+                    html.Div(className='eight columns div-for-charts bg-grey',
+                            children=[
+                                dcc.Graph(id="sales_employee", figure=fig_employee)
+                            ]
+                    ),
+                ]
+            )
+        ]
+)
+
+# ***************************************
+# Callbacks
+# ***************************************
+# Output er diagrammet
+# Input er DropDown
+@app.callback(Output('sales_employee', 'figure'),
+              [Input('drop_month', 'value')],
+              [Input('drop_year', 'value')])
+
+def update_graph(drop_month, drop_year):
+
+    print(drop_month)
+    print(drop_year)
+
+    if bool(drop_month) == False or bool(drop_year) == False:
+        order_fig1 = order
+    
+    if bool(drop_month) == True or drop_year == 'None':
+        order_fig1 = order.loc[order['ordermonth'] == drop_month]
+
+    if drop_month == 'None' or bool(drop_year) == True:
+        order_fig1 = order.loc[order['orderyear'] == drop_year]
+
+    if bool(drop_month) == True or bool(drop_year) == True:
+        order_fig1 = order.loc[order['ordermonth'] == drop_month] & order.loc[order['orderyear'] == drop_year]
+
+
+#    if drop_month and drop_year:
+#       order_fig1 = order.loc[order['ordermonth'] == drop_month] & order.loc[order['orderyear'] == drop_year]
+#    else:
+#        order_fig1 = order
+#
+#    if drop_year:
+#        order_fig1 = order.loc[order['orderyear'] == drop_year]
+#    else:
+#       order_fig1 = order
+        
+
+    return {'data':[go.Bar(
+        x = order_fig1['productname'],
+        y = order_fig1['total']
+            )
+        ]
+    }
+
+# ***************************************
+# Run the app
+# ***************************************
+if __name__ == '__main__':
+    app.run_server(debug=True)
